839c4fd4ace2b82f7e4b98d6d5ab25a9266b4bed
kixixixixi <kixixixixi@gmail.com>
2022-03-09 20:48:48 +0900

    Add nav menu to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,51 @@
 import React, { ReactElement } from "react"
+import { primaryColorHex } from "utils/constant"
+import Styled from "styled-components"
 
-const Header = (): ReactElement => {
+const Header = Styled.header`
+  background-color: ${primaryColorHex};
+  color: #eee;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  h1 {
+    font-size: 1rem;
+    margin: 0;
+  }
+  ul {
+    display: flex;
+    flex-grow: 1;
+    justify-content: space-around;
+    padding: 0 1rem;
+    width: 100%;
+    li {
+      list-style: none;
+      a {
+        color: #eee;
+        text-decoration: none;
+      }
+    }
+  }
+`
+
+const HeaderSection = (): ReactElement => {
   return (
     <>
-      <header>
-        ヘッダ
-        <nav>ナビゲーション</nav>
-      </header>
+      <Header>
+        <h1>FFFFF Cafe</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">アクセス</a>
+            </li>
+            <li>
+              <a href="/">カレンダー</a>
+            </li>
+          </ul>
+        </nav>
+      </Header>
     </>
   )
 }
 
-export default Header
+export default HeaderSection

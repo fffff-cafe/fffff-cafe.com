43cf953727e617a562baf043d3b7d28be300a45a
kixixixixi <kixixixixi@gmail.com>
2022-03-09 21:04:20 +0900

    Fix title position at cover image

--- components/cover-image-section.tsx
+++ components/cover-image-section.tsx
@@ -5,12 +5,16 @@ const CoverBackground = Styled.div`
 background-image: url(/images/cover.jpg);
 background-size: cover;
 background-position: center;
+display: flex;
 height: 50vh;
 width: 100%;
 `
 
 const CoverContent = Styled.div`
 background-color: rgba(0, 0, 0, 0.666);
+display: flex;
+flex-direction: column;
+justify-content: space-around;
 height: 100%;
 position: relative;
 width: 100%;
@@ -18,9 +22,8 @@ width: 100%;
 
 const CoverTitle = Styled.section`
 color: #eee;
-height: 100%;
+height: 33%;
 margin: 0;
-padding-top: 50%;
 position: absolute;
 text-align: center;
 width: 100%;

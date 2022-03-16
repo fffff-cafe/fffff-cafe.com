7e8d99917f45d8094e12295a639751d7f4681eda
kixixixixi <kixixixixi@gmail.com>
2022-03-16 20:07:03 +0900

    Add google calendar

--- components/calendar-section.tsx
+++ components/calendar-section.tsx
@@ -1,11 +1,21 @@
 import React, { ReactElement } from "react"
+import { Section, SectionTitle } from "components/elements"
 
 const CalendarSection = (): ReactElement => {
   return (
     <>
-      <section>
-        <h2>営業カレンダー</h2>
-      </section>
+      <Section>
+        <SectionTitle>営業カレンダー</SectionTitle>
+        <iframe
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Asia%2FTokyo&showNav=0&showPrint=0&showTabs=0&mode=AGENDA&showCalendars=0&showTz=0&showDate=0&showTitle=0&src=ZGJvZnNqY2U1bTk0bHViamJxMjhiaG4ybzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23E67C73"
+          style={{ border: "none" }}
+          width="800"
+          height="600"
+          frameBorder="0"
+          scrolling="no"
+          title="calendar"
+        ></iframe>
+      </Section>
     </>
   )
 }

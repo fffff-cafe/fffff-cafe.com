8f8ca4d8d58b2cf070301ac20b62b16036f74252
kixixixixi <kixixixixi@gmail.com>
2022-03-17 17:05:08 +0900

    Add photos to mood section

--- components/mood-section.tsx
+++ components/mood-section.tsx
@@ -1,11 +1,45 @@
 import React, { ReactElement } from "react"
+import { Section, SectionTitle } from "components/elements"
+import Styled from "styled-components"
+
+const PhotoGrid = Styled.div`
+align-items: center;
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-around;
+margin: auto;
+max-width: 640px;
+
+.item {
+  aspect-ratio: 1;
+  background-image: url(https://lh3.googleusercontent.com/p/AF1QipOCPxXXplJ4hFEnmGz-J43yxlZxeh1AhxpJUtIz=w960-h960-n-o-v1);
+  background-position: center;
+  background-size: cover;
+  width: min(33%, 33vw);
+  &:not(:hover)::after {
+    background-color: rgba(0, 0, 0, 0.3);
+    content: "";
+    display: block;
+    height: 100%;
+    width: 100%;
+  }
+}
+`
+
+const photos = Array(12).fill(undefined)
 
 const MoodSection = (): ReactElement => {
   return (
     <>
-      <section>
-        <h2>雰囲気</h2>
-      </section>
+      <Section>
+        <SectionTitle>写真</SectionTitle>
+        <PhotoGrid>
+          {photos.map((_, i) => (
+            <div key={i} className="item"></div>
+          ))}
+        </PhotoGrid>
+      </Section>
     </>
   )
 }

988fecfdaae44dc87dfbb410acd2e65879b271e7
kixixixixi <kixixixixi@gmail.com>
2023-03-27 02:36:57 +0900

    Add slack invite url

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -1,22 +1,40 @@
 import React, { ReactElement } from "react"
 import { Section, SectionTitle } from "components/elements"
 
+const Link: React.FC<
+  React.DetailedHTMLProps<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  >
+> = ({ children, href }) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+      style={{ color: "white", margin: "0.25rem" }}
+    >
+      {children}
+    </a>
+  )
+}
+
 const ContactSection = (): ReactElement => {
   return (
     <>
-      <Section>
+      <Section style={{ textAlign: "center" }}>
         <SectionTitle>お問い合わせ</SectionTitle>
-        <p style={{ textAlign: "center" }}>
-          <a
-            style={{ color: "white", margin: "0.25rem" }}
-            href="//twitter.com/fffff_cafe"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Twitter
-          </a>
+        <div>
+          <Link href="//twitter.com/fffff_cafe">Twitter</Link>
           のDMからご連絡ください。
-        </p>
+        </div>
+        <div>
+          イベントの連絡については
+          <Link href="//join.slack.com/t/fffff-81d6773/shared_invite/zt-1d2ozux7o-F2O5pNC5vmXzt1Allox2xQ">
+            こちらのSlack
+          </Link>
+          からも受け付けています。
+        </div>
       </Section>
     </>
   )

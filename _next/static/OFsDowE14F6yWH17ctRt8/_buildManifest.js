18e34f824ad3d027ed6b11c09604bbce6f7791dc
kixixixixi <kixixixixi@users.noreply.github.com>
2025-05-14 05:26:25 +0000

    deploy: 61268da0b222e3e3eac4e4d293105a79d1aceae8

--- _next/static/OFsDowE14F6yWH17ctRt8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/799-79c1502059ed53a3.js","static/css/55278fc833c7a365.css","static/chunks/pages/index-feeeeb80264a7ec3.js"],"/_error":["static/chunks/pages/_error-9b2af70ec087ba46.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

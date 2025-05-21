c4997977beb1aa95572ce329fac7f245050f346d
kixixixixi <kixixixixi@gmail.com>
2025-05-21 17:52:58 +0900

    Remove menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import ConceptSection from "components/concept-section"
 import CoverImageSection from "components/cover-image-section"
 import FooterSection from "components/footer-section"
 import Header from "components/header"
-import MenuSection from "components/menu-section"
 import MoodSection from "components/mood-section"
 import NowSection from "components/now-section"
 import SystemSection from "components/system-section"
@@ -20,7 +19,6 @@ const IndexPage = (): React.ReactElement => {
       <SystemSection />
       <MoodSection />
       <CalendarSection />
-      <MenuSection />
       <MemberSection />
       <NowSection />
       <AccessSection />
